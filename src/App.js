5be8a6c1a88e9137adcca6af32fa2e212eca2598
ruzina75@gmail.com <ruzina75@gmail.com>
2020-05-18 19:37:15 +0100

    Updated Default state for modal popup

--- src/App.js
+++ src/App.js
@@ -2,41 +2,67 @@ import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 import Header from "./Header/Header";
-import ListTitle from "./ListTitle/ListTitle";
-import AddTaskItem from "./AddTaskItem/AddTaskItem";
-import TaskItem from "./TaskItem/TaskItem";
+import List from "./List/List";
 import NewList from "./CreateNewList/NewList";
 
 function App() {
-  const [tasks, setTasks] = useState([
-    { text: "Paint and oil outdoor furniture", completed: true, id: uuidv4(), dateCreated: Date.now() },
-    { text: "Hang pictures in bedroom", completed: false, id: uuidv4(), dateCreated: Date.now() },
-    { text: "Order party decorations", completed: false, id: uuidv4(), dateCreated: Date.now() },
-    { text: "Order replacement printer toner", completed: false, id: uuidv4(), dateCreated: Date.now() },
-    { text: "Organise Metrolink refund", completed: true, id: uuidv4(), dateCreated: Date.now() },
+  const [lists, setLists] = useState([
+    {
+      title: "Shopping List",
+      id: uuidv4(),
+      dateCreated: Date.now(),
+      tasks: [
+        { text: "Paint and oil outdoor furniture", completed: true, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Hang pictures in bedroom", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Order party decorations", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Order replacement printer toner", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Organise Metrolink refund", completed: true, id: uuidv4(), dateCreated: Date.now() },
+      ],
+    },
+    {
+      title: "Shopping List 2",
+      id: uuidv4(),
+      dateCreated: Date.now(),
+      tasks: [
+        { text: "Paint and oil outdoor furniture", completed: true, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Hang pictures in bedroom", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Order party decorations", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Order replacement printer toner", completed: false, id: uuidv4(), dateCreated: Date.now() },
+        { text: "Organise Metrolink refund", completed: true, id: uuidv4(), dateCreated: Date.now() },
+      ],
+    },
   ]);
-
-  //sort tasks by date created then put the uncompleted tasks at the top
-  tasks.sort((a, b) => a.dateCreated - b.dateCreated).sort(({ completed }) => (completed ? 1 : -1));
 
   //create add task function - console.log("testing")
   const addTask = (task) => {
-    setTasks([{ text: task, completed: false, id: uuidv4(), dateCreated: Date.now() }, ...tasks]);
+    console.log("adding");
+    //setLists([{ text: task, completed: false, id: uuidv4(), dateCreated: Date.now() }, ...lists]);
   };
 
   function deleteTask(id) {
-    const updatedTasks = tasks.filter((task) => task.id !== id);
-    setTasks(updatedTasks);
+    const updatedTasks = lists.map((list) => {
+      const { tasks = [] } = list;
+      return { ...list, tasks: tasks.filter(({ id: taskid }) => taskid !== id) };
+    });
+    setLists(updatedTasks);
   }
 
   function toggleTask(id) {
-    const updatedTasks = tasks.map((task) => {
-      if (task.id === id) {
-        task.completed = !task.completed;
-      }
-      return task;
+    const updatedTasks = lists.map((list) => {
+      const { tasks = [] } = list;
+      // if (task.id === id) {
+      //   task.completed = !task.completed;
+      // }
+      return {
+        ...list,
+        tasks: tasks.map(({ id: taskid }) => {
+          if (taskid === id) {
+            tasks.completed = !tasks.completed;
+          }
+        }),
+      };
     });
-    setTasks(updatedTasks);
+    setLists(updatedTasks);
   }
 
   return (
@@ -46,10 +72,8 @@ function App() {
         <div className="row">
           <div className="col-12 col-sm-6 col-md-4">
             <NewList />
-            <ListTitle />
-            <AddTaskItem addTask={addTask} />
-            {tasks.map(({ text = "", completed = false, id }) => (
-              <TaskItem key={id} id={id} text={text} completed={completed} deleteTask={deleteTask} toggleTask={toggleTask} />
+            {lists.map(({ title = "", tasks = [] }) => (
+              <List title={title} tasks={tasks} deleteTask={deleteTask} toggleTask={toggleTask} addTask={addTask} />
             ))}
           </div>
         </div>
