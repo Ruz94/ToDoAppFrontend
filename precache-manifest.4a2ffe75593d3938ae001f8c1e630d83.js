91c42b696bf64611a680d7f8f658bb4e8f8768fc
ruzina75@gmail.com <ruzina75@gmail.com>
2020-08-12 14:35:47 +0100

    Updates

---
+++ precache-manifest.4a2ffe75593d3938ae001f8c1e630d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a9a71e8c4763870375638a6f52d0f6",
+    "url": "/ToDoAppFrontend/index.html"
+  },
+  {
+    "revision": "918b6b4577e725d57b21",
+    "url": "/ToDoAppFrontend/static/css/main.3d86d587.chunk.css"
+  },
+  {
+    "revision": "35d39ba03b9584118f02",
+    "url": "/ToDoAppFrontend/static/js/2.83be2657.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ToDoAppFrontend/static/js/2.83be2657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "918b6b4577e725d57b21",
+    "url": "/ToDoAppFrontend/static/js/main.486a29a7.chunk.js"
+  },
+  {
+    "revision": "879108933fc395333b35",
+    "url": "/ToDoAppFrontend/static/js/runtime-main.8e86ca32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDoAppFrontend/precache-manifest.d368364ed5912d7eef89b21c1c66172b.js"
+  "/ToDoAppFrontend/precache-manifest.4a2ffe75593d3938ae001f8c1e630d83.js"
 );
 
 self.addEventListener('message', (event) => {
